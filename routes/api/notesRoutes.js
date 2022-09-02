88d7a12d22a6fb29113d23d15ab16a7c981588c1
Dragon Lore <tate.j.foster@gmail.com>
2022-09-02 16:34:32 -0600

    added helper code for reading/writing to db.json got code for posting to api/notes

--- routes/api/notesRoutes.js
+++ routes/api/notesRoutes.js
@@ -1,7 +1,26 @@
 const notes = require("express").Router();
+const { readFromFile, readAndAppend } = require("../../helper/fsUtils");
 
 notes.get("/", (req, res) => {
 	readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+notes.post("/", (req, res) => {
+	console.log(req.body);
+
+	const { title, text } = req.body;
+
+	if (req.body) {
+		const newNote = {
+			title,
+			text,
+		};
+
+		readAndAppend(newNote, "./db/db.json");
+		res.json(`New note added!`);
+	} else {
+		res.errored(`Error in adding note`);
+	}
 });
 
 module.exports = notes;
--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", api);
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
